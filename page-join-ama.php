c656587a1b052aeea74cc94ee39859ba58d1dcce
BenKincaid <bkincaid1@toromail.csudh.edu>
2017-03-07 17:27:32 -0800

    commit 3/7/17

--- page-join-ama.php
+++ page-join-ama.php
@@ -30,31 +30,32 @@ include "parts/page-header.php"; ?>
 
 			<div class="container">
 
-				<form method="post" action = "<?php $_SERVER['PHP_SELF']; ?>">
-					<h3>Name:</h3>
+				<form method="post" class = "form-join" action = "<?php $_SERVER['PHP_SELF']; ?>">
+                    <h2 class = "form-header-text">Please fill out this form and a ToroAMA board member will be in touch with you shortly!</h2>
+					<h3 class = "form-text">Name:</h3>
 					<input type="text" name = "contact-name"  value="<?php echo $name ?>">
 						<span class="error"><?php echo $name_error; ?></span>
-						<h3>Email:</h3>
+						<h3 class = "form-text">Email:</h3>
 					<input type="text" name = "email-name"  value="<?php echo $email ?>">
 						<span class="error"><?php echo $email_error; ?></span>
 
-						<h3>CSUDH Identification Number:</h3>
+						<h3 class = "form-text">CSUDH Identification Number:</h3>
 	 				 <input type="text" name = "number-name"  value="<?php echo $number ?>">
 	 				 	<span class="error"><?php echo $number_error; ?></span>
 
 
-						<h3>Major:</h3>
+						<h3 class = "form-text">Major:</h3>
 											<input type="text" name = "major-name"  value="<?php echo $major ?>">
 						<span class="error"><?php echo $name_error; ?></span>
 
-						<h3>Reason for joining AMA:</h3>
+						<h3 class = "form-text">Reason for joining AMA:</h3>
 						<select name = "SelectReason[]" multiple>
 							<option value="one">Professional Development</option>
 							<option value="two">Workshops</option>
 							<option value="three">Networking</option>
 							<option value="four">Other</option>
 							<select>
-								<input type="submit" name="submit" value="submit">
+								<input class="submit-input" type="submit" name="submit" value="submit">
 					<div class="success"><?php echo $success ?></div>
 				</form>
 			</div>
